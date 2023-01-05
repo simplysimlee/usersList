0fb1b850edcb9e2fa4e67b235a562e4e6f7cfda0
Das <simlee.das@cgi.com>
2023-01-05 17:12:27 +0100

    test cases

--- user-app/src/app/pipes/user-filter.pipe.spec.ts
+++ user-app/src/app/pipes/user-filter.pipe.spec.ts
@@ -7,23 +7,68 @@ describe('UserFilterPipe', () => {
   });
 });
 
-describe('UserFilterPipe', () => {
+describe('UserFilterPipe - Positive Test Scenarios', () => {
  
   let pipe: UserFilterPipe;
-
   beforeEach(()=>{
     pipe = new UserFilterPipe();
   });
+  const user = [
+    {
+      "id":1,
+      "displayName":"John One",
+      "givenName":"John",
+      "mail":"John@one.com",
+      "details":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum. Dolor."
+    },
+    {
+      "id":2,
+      "displayName":"John Two",
+      "givenName":"John",
+      "mail":"John@two.com",
+      "details":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum. Dolor."
+    }]
 
-  it('return same value if undefined', () => {
+  it('return [] if user list is empty', () => {
     expect(pipe.transform([],null)).toBe([]);
   });
 
-  it('returns a number if ID matches', () => {
-  //  const actualValue = pipe.transform(5);
-
-    //expect(pipe.transform(,null)).toBe([]);
+  it('return same value if argument is null', () => {
+    expect(pipe.transform(user,null)).toBe(user);
   });
+
+  it('returns a filtered value if ID matches', () => {
+   const actualValue = pipe.transform(user,2);
+    expect(actualValue).toBe([{
+      "id":2,
+      "displayName":"John Two",
+      "givenName":"John",
+      "mail":"John@two.com",
+      "details":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum. Dolor."
+    }]);
+  });
+
+  it('returns a filtered value if ID matches', () => {
+    const actualValue = pipe.transform(user,2);
+     expect(actualValue).toBe([{
+       "id":2,
+       "displayName":"John Two",
+       "givenName":"John",
+       "mail":"John@two.com",
+       "details":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum. Dolor."
+     }]);
+   });
+
+   it('returns a filtered value if given name matches', () => {
+    const actualValue = pipe.transform(user,'john');
+     expect(actualValue).toBe([{
+       "id":2,
+       "displayName":"John Two",
+       "givenName":"John",
+       "mail":"John@two.com",
+       "details":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum. Dolor."
+     }]);
+   });
 
 
 });
